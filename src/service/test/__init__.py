7b63752fc025abb578c019cebd8a979c3d1b9fad
Oliver Catling <ollicatling@gmail.com>
2021-10-10 19:12:27 +0100

    added test resources

--- src/service/test/__init__.py
+++ src/service/test/__init__.py
@@ -2,5 +2,5 @@ import json
 
 
 def get_example_raw():
-    with open('./src/service/test/resources/fpl_data.json', 'r') as infile:
+    with open('./src/service/test/resources/example_raw_data.json', 'r') as infile:
         return json.loads(infile.read())
