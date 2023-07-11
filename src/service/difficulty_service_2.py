ea2c6ee64615b396a2944d6594daaac08ed44cb5
Oliver Catling <oliver.catling@elemica.com>
2023-07-11 15:56:09 +0100

    season now a property of difficulty service

--- src/service/difficulty_service_2.py
+++ src/service/difficulty_service_2.py
@@ -2,10 +2,22 @@ from functools import reduce
 
 from src.model.fixture import Fixture
 from src.model.season import Season
+from src.model.team import Team
 
 
 class DifficultyService:
-    def __init__(self, season: Season):
+    def __init__(self, season: Season, teams_to_exclude: list[Team] = None):
+        self._season = season
+        if not teams_to_exclude:
+            teams_to_exclude = []
+        self._teams_to_exclude = teams_to_exclude
+
+    @property
+    def season(self) -> Season:
+        return self._season
+
+    @season.setter
+    def season(self, season: Season) -> None:
         self._season = season
 
     @staticmethod
@@ -17,12 +29,12 @@ class DifficultyService:
         return reduce(lambda x, y: x + y.away_team_difficulty, fixtures, 0)
 
     def get_team_difficulty_for_home_games(self, team_id: int, starting_week: int = 0, finish_week: int = 38) -> int:
-        fixtures = self._season.organise_fixtures_by_team(starting_week, finish_week)
+        fixtures = self.season.organise_fixtures_by_team(starting_week, finish_week)
         team_home_fixtures: list[Fixture] = fixtures[team_id]["home_games"]
         return self.get_difficulty_from_home_fixtures(team_home_fixtures)
 
     def get_team_difficulty_for_away_games(self, team_id: int, starting_week: int = 0, finish_week: int = 38) -> int:
-        fixtures = self._season.organise_fixtures_by_team(starting_week, finish_week)
+        fixtures = self.season.organise_fixtures_by_team(starting_week, finish_week)
         team_away_fixtures: list[Fixture] = fixtures[team_id]["away_games"]
         return self.get_difficulty_from_away_fixtures(team_away_fixtures)
 
@@ -31,7 +43,7 @@ class DifficultyService:
             self.get_team_difficulty_for_away_games(team_id, starting_week, finish_week)
 
     def get_all_teams_period_difficulty(self, starting_week: int = 0, finish_week: int = 38):
-        fixtures_organised_by_team = self._season.organise_fixtures_by_team(starting_week, finish_week)
+        fixtures_organised_by_team = self.season.organise_fixtures_by_team(starting_week, finish_week)
         for team_id, team_data in fixtures_organised_by_team.items():
             home_difficulty = self.get_difficulty_from_home_fixtures(team_data["home_games"])
             away_difficulty = self.get_difficulty_from_away_fixtures(team_data["away_games"])
