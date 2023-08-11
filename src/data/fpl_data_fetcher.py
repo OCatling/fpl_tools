a05007ecef317c96f24b6a3d81350ed60ca7d69c
Oliver Catling <oliver.catling@elemica.com>
2023-08-11 15:12:22 +0100

    filename now a parameter when fetching data

--- src/data/fpl_data_fetcher.py
+++ src/data/fpl_data_fetcher.py
@@ -12,16 +12,20 @@ RAW_FILE_NAME = os.path.join(DOWNLOAD_PATH, 'fpl_data.json')
 FIXTURE_FILE_NAME = os.path.join(DOWNLOAD_PATH, 'fpl_fixtures.json')
 
 
-def download_raw() -> None:
+def download_raw(raw_file_name="") -> None:
+    if not raw_file_name:
+        raw_file_name = RAW_FILE_NAME
     response = requests.get(
         url=FPL_RAW_DATA_ENDPOINT,
         params=None
     )
-    with open(RAW_FILE_NAME, 'w') as outfile:
+    with open(raw_file_name, 'w') as outfile:
         outfile.write(response.text)
 
 
-def download_fixtures(gameweek=0) -> None:
+def download_fixtures(fixtures_file_name="", gameweek=0) -> None:
+    if not fixtures_file_name:
+        fixtures_file_name = FIXTURE_FILE_NAME
     params = None
     if gameweek > 0:
         params = {'event': gameweek}
@@ -29,19 +33,23 @@ def download_fixtures(gameweek=0) -> None:
         url=FPL_FIXTURES_ENDPOINT,
         params=params
     )
-    with open(FIXTURE_FILE_NAME, 'w') as outfile:
+    with open(fixtures_file_name, 'w') as outfile:
         outfile.write(response.text)
 
 
-def get_raw() -> dict:
-    if not os.path.exists(RAW_FILE_NAME):
+def get_raw(raw_file_name) -> dict:
+    if not raw_file_name:
+        raw_file_name = RAW_FILE_NAME
+    if not os.path.exists(raw_file_name):
         download_raw()
-    with open(RAW_FILE_NAME, 'r') as infile:
+    with open(raw_file_name, 'r') as infile:
         return json.loads(infile.read())
 
 
-def get_fixtures(gameweek=0) -> list[dict]:
-    if not os.path.exists(FIXTURE_FILE_NAME):
-        download_fixtures(gameweek)
-    with open(FIXTURE_FILE_NAME, 'r') as infile:
+def get_fixtures(fixtures_file_name="", gameweek=0) -> list[dict]:
+    if not fixtures_file_name:
+        fixtures_file_name = FIXTURE_FILE_NAME
+    if not os.path.exists(fixtures_file_name):
+        download_fixtures(fixtures_file_name, gameweek)
+    with open(fixtures_file_name, 'r') as infile:
         return json.loads(infile.read())
